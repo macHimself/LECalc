77225731d99554bb5dea00914644a308ec4c7ae0
Adam <54817317+macHimself@users.noreply.github.com>
2024-04-28 10:30:27 +0200

    test for roman numbers
    UI test

--- leCalc/leCalcUITests/leCalcUITestsLaunchTests.swift
+++ leCalc/leCalcUITests/leCalcUITestsLaunchTests.swift
@@ -32,20 +32,37 @@ final class leCalcUITestsLaunchTests: XCTestCase {
         add(attachment)
     }*/
     
-    func testOne() {
-        
-        
-        
+    func testRomanNumbersConvertor() {
+        // APP it self
         let app = XCUIApplication()
+        
         app.launch()
         
+        // go to converter R<-->A numbers
         app.collectionViews/*@START_MENU_TOKEN@*/.buttons["Roman Numbers"]/*[[".cells.buttons[\"Roman Numbers\"]",".buttons[\"Roman Numbers\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        // check visible messages
         XCTAssertTrue(app.staticTexts["please, insert number 0-3999"].exists)
+        XCTAssertTrue(app.staticTexts["!!!0!!!"].exists)
+        
+        //check invisible description by openin and closing it
+        app.buttons[">>>"].tap()
+                // too long text need to work over it limit for static text is 128 chars.
+        //XCTAssertTrue failed: throwing "NSInternalInconsistencyException: Invalid query - string identifier 'Roman numerals are a numeral system that originated in ancient Rome and remained the usual way of writing numbers throughout Europe well into the Late Middle Ages. Numbers are written with combinations of letters from the Latin alphabet, each letter with a fixed integer value. Modern style uses only these seven: I = 1 .. V = 5 .. X = 10 .. L = 50 .. C = 100 .. D = 500 .. M = 1000' exceeds maximum length of 128 characters. You can work around this limitation by constructing a query with a custom NSPredicate that specifies the property (label, title, value, placeholderValue, or identifier) to match against."
+        
+              //  XCTAssertTrue(app.staticTexts["Roman numerals are a numeral system that originated in ancient Rome and remained the usual way of writing numbers throughout Europe well into the Late Middle Ages. Numbers are written with combinations of letters from the Latin alphabet, each letter with a fixed integer value. Modern style uses only these seven: I = 1 .. V = 5 .. X = 10 .. L = 50 .. C = 100 .. D = 500 .. M = 1000"].exists)
+        app.buttons["<<<"].tap()
+        
+        // type arabic number
         app.textFields["arabic number"].tap()
         app.textFields["arabic number"].typeText("123")
+        // check result in roman number
         XCTAssertTrue(app.staticTexts["CXXIII"].exists)
         
-        
-        
+        // type roman number
+        app.textFields["roman number"].tap()
+        app.textFields["roman number"].typeText("MM")
+        // check resut in arabic number
+        XCTAssertTrue(app.staticTexts["2000"].exists)
     }
 }
